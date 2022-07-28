c0e414d50fb567f977f6ca01c548429c589332b2
brunobonaldi94 <brunobonaldi94@gmail.com>
2022-07-28 04:34:30 +0200

    adding cmd parser

--- mandatory_srcs/includes/pipex.h
+++ mandatory_srcs/includes/pipex.h
@@ -15,6 +15,30 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
 # include "../libft/libft.h"
+
+# define TRUE 1
+# define FALSE 0
+
+# define ERROR_CODE 1
+# define SUCESS_CODE 0
+
+# define TOO_FEW_ARGUMENTS_ERROR_MSG "Pipex must be run with at least \
+'file1' 'cmd1' 'cmd2' 'file2' arguments"
+
+typedef struct s_file
+{
+	int		fd;
+	char	*file_name;
+}	t_file;
+
+typedef struct s_args
+{
+	int		argc;
+	t_file	input_file;
+	char	**commands;
+	t_file	output_file;
+}	t_args;
 
 #endif
--- pipex.c
+++ pipex.c
@@ -12,7 +12,37 @@
 
 #include "pipex.h"
 
-int	main(void)
+void	exit_with_message(int status_code, char *message)
 {
+	ft_printf(message);
+	exit(status_code);
+}
+
+// int	parse_args(int argc, char *argv[], t_args *args)
+// {
+// 	if (args->argc <= 1)
+// 		exit_with_message(ERROR_CODE, TOO_FEW_ARGUMENTS_ERROR_MSG);
+// 	ft_printf(argv[1]);
+// 	return (0);
+// }
+
+int	load_args(int argc, char *argv[], t_args *args)
+{
+	if (args->argc <= 1)
+		exit_with_message(ERROR_CODE, TOO_FEW_ARGUMENTS_ERROR_MSG);
+	args->argc = argc;
+	args->input_file.file_name = argv[1];
+	args->input_file.fd = open(args->input_file.file_name, O_RDONLY);
+	args->output_file.file_name = argv[argc - 1];
+	args->output_file.fd = open(args->output_file.file_name, O_RDONLY);
+	ft_printf("%d", argc);
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	t_args	args;
+
+	load_args(argc, argv, &args);
 	return (0);
 }
