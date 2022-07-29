0fa9c522632faa47604956a96766d8aa7a32ca79
brunobonaldi94 <brunobonaldi94@gmail.com>
2022-07-29 04:02:40 +0200

    parsing aggs

--- mandatory_srcs/includes/pipex.h
+++ mandatory_srcs/includes/pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbonaldi <bbonaldi@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 23:07:26 by bbonaldi          #+#    #+#             */
-/*   Updated: 2022/07/26 23:08:30 by bbonaldi         ###   ########.fr       */
+/*   Updated: 2022/07/29 02:41:50 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,14 @@
 # define ERROR_CODE 1
 # define SUCESS_CODE 0
 
+# define RED "\033[0;31m"
+# define BLUE "\033[0;34m"
+# define BLACK "\033[0;30m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
 # define TOO_FEW_ARGUMENTS_ERROR_MSG "Pipex must be run with at least \
 'file1' 'cmd1' 'cmd2' 'file2' arguments"
+# define FILE_NOT_FOUND_ERROR_MSG "File does not exist!"
 
 typedef struct s_file
 {
@@ -36,6 +42,8 @@ typedef struct s_file
 typedef struct s_args
 {
 	int		argc;
+	char	**argv;
+	char	**envp;
 	t_file	input_file;
 	char	**commands;
 	t_file	output_file;
--- pipex.c
+++ pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbonaldi <bbonaldi@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 22:59:50 by bbonaldi          #+#    #+#             */
-/*   Updated: 2022/07/26 23:18:12 by bbonaldi         ###   ########.fr       */
+/*   Updated: 2022/07/29 02:49:46 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,56 @@
 
 void	exit_with_message(int status_code, char *message)
 {
-	ft_printf(message);
+	if (status_code == ERROR_CODE)
+		ft_printf(RED"%s\n",message);
+	else
+		ft_printf(GREEN"%s\n",message);
 	exit(status_code);
 }
 
-// int	parse_args(int argc, char *argv[], t_args *args)
-// {
-// 	if (args->argc <= 1)
-// 		exit_with_message(ERROR_CODE, TOO_FEW_ARGUMENTS_ERROR_MSG);
-// 	ft_printf(argv[1]);
-// 	return (0);
-// }
-
-int	load_args(int argc, char *argv[], t_args *args)
+void	print_arg_error(char *arg, int status_code, char *message)
 {
-	if (args->argc <= 1)
-		exit_with_message(ERROR_CODE, TOO_FEW_ARGUMENTS_ERROR_MSG);
-	args->argc = argc;
-	args->input_file.file_name = argv[1];
+		ft_printf("arg: %s - ", arg);
+		exit_with_message(status_code, message);
+}
+
+void	load_file(t_args *args)
+{
+	args->input_file.file_name = args->argv[1];
 	args->input_file.fd = open(args->input_file.file_name, O_RDONLY);
-	args->output_file.file_name = argv[argc - 1];
+	args->output_file.file_name = args->argv[args->argc - 1];
 	args->output_file.fd = open(args->output_file.file_name, O_RDONLY);
-	ft_printf("%d", argc);
+	if (args->input_file.fd < 0)
+		print_arg_error(args->input_file.file_name,
+			ERROR_CODE, FILE_NOT_FOUND_ERROR_MSG);
+	if (args->output_file.fd < 0)
+		print_arg_error(args->output_file.file_name,
+			ERROR_CODE, FILE_NOT_FOUND_ERROR_MSG);
+}
+
+void	load_args(int argc, char *argv[], char *envp[], t_args *args)
+{
+	args->argc = argc;
+	args->argv = argv;
+	args->envp = envp;
+}
+
+int	parse_args(t_args *args)
+{
+	if (args->argc <= 4)
+		exit_with_message(ERROR_CODE, TOO_FEW_ARGUMENTS_ERROR_MSG);
+	load_file(args);
+	ft_printf("%d | %d", args->input_file.fd, args->output_file.fd);
+	close(args->input_file.fd);
+	close(args->output_file.fd);
 	return (0);
 }
 
-int	main(int argc, char *argv[])
+int	main(int argc, char *argv[], char *envp[])
 {
 	t_args	args;
 
-	load_args(argc, argv, &args);
+	load_args(argc, argv, envp, &args);
+	parse_args(&args);
 	return (0);
 }
