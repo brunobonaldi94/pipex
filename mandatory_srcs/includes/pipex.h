c664462d04fc2cb2ef660a1e8bd69a4c599fb81c
brunobonaldi94 <brunobonaldi94@gmail.com>
2022-08-05 05:35:06 +0200

    added parser

--- mandatory_srcs/includes/pipex.h
+++ mandatory_srcs/includes/pipex.h
@@ -24,11 +24,23 @@
 # define ERROR_CODE 1
 # define SUCESS_CODE 0
 
+# define READ_FD 0
+# define WRITE_FD 1
+
+# define STDIN 0
+# define STDOUT 1
+
+
 # define RED "\033[0;31m"
 # define BLUE "\033[0;34m"
 # define BLACK "\033[0;30m"
 # define GREEN "\033[0;32m"
 # define YELLOW "\033[0;33m"
+
+# define INDEX_COMMANDS_START 2
+# define COMMANDS_OFFSET 3
+# define MINIMUM_NUMBER_ARGS 4
+
 # define TOO_FEW_ARGUMENTS_ERROR_MSG "Pipex must be run with at least \
 'file1' 'cmd1' 'cmd2' 'file2' arguments"
 # define FILE_NOT_FOUND_ERROR_MSG "File does not exist!"
@@ -39,14 +51,24 @@ typedef struct s_file
 	char	*file_name;
 }	t_file;
 
-typedef struct s_args
+typedef struct s_cmd
+{
+	char	**cmd_to_parse;
+	char	*cmd;
+	char	**argv;
+}	t_cmd;
+
+typedef struct s_arguments
 {
 	int		argc;
 	char	**argv;
 	char	**envp;
+	int		**fd_pipes;
+	pid_t	*pids_fork;
+	int		number_commands;
+	t_cmd	*commands;
 	t_file	input_file;
-	char	**commands;
 	t_file	output_file;
-}	t_args;
+}	t_arguments;
 
 #endif
--- pipex.c
+++ pipex.c
@@ -27,43 +27,106 @@ void	print_arg_error(char *arg, int status_code, char *message)
 		exit_with_message(status_code, message);
 }
 
-void	load_file(t_args *args)
+void	load_file(t_arguments *arguments)
 {
-	args->input_file.file_name = args->argv[1];
-	args->input_file.fd = open(args->input_file.file_name, O_RDONLY);
-	args->output_file.file_name = args->argv[args->argc - 1];
-	args->output_file.fd = open(args->output_file.file_name, O_RDONLY);
-	if (args->input_file.fd < 0)
-		print_arg_error(args->input_file.file_name,
+	arguments->input_file.file_name = arguments->argv[1];
+	arguments->input_file.fd = open(arguments->input_file.file_name, O_RDONLY);
+	arguments->output_file.file_name = arguments->argv[arguments->argc - 1];
+	arguments->output_file.fd = open(arguments->output_file.file_name, O_RDONLY);
+	if (arguments->input_file.fd < 0)
+		print_arg_error(arguments->input_file.file_name,
 			ERROR_CODE, FILE_NOT_FOUND_ERROR_MSG);
-	if (args->output_file.fd < 0)
-		print_arg_error(args->output_file.file_name,
+	if (arguments->output_file.fd < 0)
+		print_arg_error(arguments->output_file.file_name,
 			ERROR_CODE, FILE_NOT_FOUND_ERROR_MSG);
 }
 
-void	load_args(int argc, char *argv[], char *envp[], t_args *args)
+void	load_args(int argc, char *argv[], char *envp[], t_arguments *arguments)
 {
-	args->argc = argc;
-	args->argv = argv;
-	args->envp = envp;
+	arguments->argc = argc;
+	arguments->argv = argv;
+	arguments->envp = envp;
 }
 
-int	parse_args(t_args *args)
+void	parse_cmd(t_cmd *cmd)
 {
-	if (args->argc <= 4)
+	int	i;
+	
+	
+	i = 0;
+	while (cmd->cmd_to_parse[i])
+		++i;
+	cmd->argv = (char **)malloc(sizeof(char *) * i);
+	cmd->cmd = cmd->cmd_to_parse[0];
+	i = 1;
+	while (cmd->cmd_to_parse[i])
+	{
+		cmd->argv[i - 1] = cmd->cmd_to_parse[i];
+		i++;
+	}
+	cmd->argv[i - 1] = NULL;
+}
+
+void	parse_commands(t_arguments *arguments)
+{
+	int	i;
+	int	j;
+
+	arguments->number_commands = arguments->argc 
+		- COMMANDS_OFFSET;
+	arguments->commands = (t_cmd *)malloc(sizeof(t_cmd) *
+		arguments->number_commands);
+	i = INDEX_COMMANDS_START;
+	j = 0;
+	while (i < arguments->argc - 1)
+	{
+		arguments->commands[j].cmd_to_parse =
+			ft_split(arguments->argv[i++], ' ');
+		parse_cmd(&arguments->commands[j++]);
+	}
+}
+
+void	free_args(t_arguments *arguments)
+{
+	int	j;
+	int	i;
+
+	j = 0;
+	i = 0;
+	while (i < arguments->number_commands)
+	{
+		j = 0;
+		while (arguments->commands[i].cmd_to_parse[j])
+		{
+			free(arguments->commands[i].cmd_to_parse[j]);
+			j++;
+		}
+		free(arguments->commands[i].argv);
+		free(arguments->commands[i].cmd_to_parse);
+		i++;
+	}	free(arguments->commands);
+}
+
+int	parse_args(t_arguments *arguments)
+{
+	if (arguments->argc <= MINIMUM_NUMBER_ARGS)
 		exit_with_message(ERROR_CODE, TOO_FEW_ARGUMENTS_ERROR_MSG);
-	load_file(args);
-	ft_printf("%d | %d", args->input_file.fd, args->output_file.fd);
-	close(args->input_file.fd);
-	close(args->output_file.fd);
+	arguments->input_file.file_name = arguments->argv[1];
+	arguments->output_file.file_name = arguments->argv[arguments->argc - 1];
+	parse_commands(arguments);
+	ft_printf("input_file: %s\n", arguments->input_file.file_name);
+	ft_printf("command 1:%s argv: %s\n", arguments->commands[0].cmd,  arguments->commands[0].argv[0]);
+	ft_printf("command 2:%s argv: %s\n", arguments->commands[1].cmd, arguments->commands[1].argv[0]);
+	ft_printf("output_file: %s\n", arguments->output_file.file_name);
+	free_args(arguments);
 	return (0);
 }
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	t_args	args;
+	t_arguments	arguments;
 
-	load_args(argc, argv, envp, &args);
-	parse_args(&args);
+	load_args(argc, argv, envp, &arguments);
+	parse_args(&arguments);
 	return (0);
 }
